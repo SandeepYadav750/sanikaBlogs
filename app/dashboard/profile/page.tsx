d638af7897525c1d6b1c9d2f00e23e5305554be9
sandeep yadav <sandeep@rechargezap.in>
2026-04-14 16:15:12 +0530

    profile auth some changes

--- app/dashboard/profile/page.tsx
+++ app/dashboard/profile/page.tsx
@@ -2,7 +2,7 @@
 import { useState } from "react";
 import { useSelector } from "react-redux";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { Card} from "@/components/ui/card";
+import { Card } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { FaFacebook, FaLinkedin } from "react-icons/fa";
@@ -11,11 +11,12 @@ import EditProfileModal from "@/components/EditProfileModal";
 import TotalProperty from "@/components/TotalProperty";
 
 const Profile = () => {
-  const { user } = useSelector((state: any) => state.auth.user);
-
+  const { user } = useSelector((state: any) => state.auth);
+  const singleUser = user?.user;
+  console.log("User data in Profile component:", singleUser);
   const [isEditModalOpen, setIsEditModalOpen] = useState(false);
 
-  const displayName = user.firstName + " " + user.lastName || " Guest User";
+  const displayName = singleUser.firstName + " " + singleUser.lastName || " Guest User";
 
   return (
     <div className="max-w-7xl mx-auto px-4 flex gap-6 bg-gray-100 dark:bg-gray-900">
@@ -23,21 +24,21 @@ const Profile = () => {
         <Card className="flex flex-col sm:flex-row items-center gap-6 p-6">
           <div className="flex flex-col items-center gap-2">
             <Avatar className="w-28 h-28">
-              <AvatarImage src={user.photoURL || "/avatar.png"} />
+              <AvatarImage src={singleUser.photoURL || "/avatar.png"} />
               <AvatarFallback>{displayName.charAt(0)}</AvatarFallback>
             </Avatar>
-            <span>{user.occupation}</span>
+            <span>{singleUser.occupation}</span>
             <div className="flex items-center justify-center gap-2 text-xl w-full">
-              <Link href={user.facebook} target="_blank">
+              <Link href={singleUser.facebook} target="_blank">
                 <FaFacebook />
               </Link>
-              <Link href={user.twitter} target="_blank">
+              <Link href={singleUser.twitter} target="_blank">
                 <FaSquareXTwitter />
               </Link>
-              <Link href={user.linkedin} target="_blank">
+              <Link href={singleUser.linkedin} target="_blank">
                 <FaLinkedin />
               </Link>
-              <Link href={user.instagram} target="_blank">
+              <Link href={singleUser.instagram} target="_blank">
                 <FaSquareInstagram />
               </Link>
             </div>
@@ -47,14 +48,14 @@ const Profile = () => {
               Welcome {displayName || "N/A"}
             </h2>
             <p className="my-2 text-sm text-gray-600 dark:text-gray-300">
-              Email : {user.email || "N/A"}
+              Email : {singleUser.email || "N/A"}
             </p>
             <p className="text-sm text-gray-700 dark:text-gray-200">
               <span className="font-medium">About Me:</span>
             </p>
             <div className=" p-4 bg-gray-200 dark:bg-gray-800 rounded">
               <p className="text-sm text-gray-700 dark:text-gray-200">
-                {user.bio}
+                {singleUser.bio}
               </p>
             </div>
             <Button onClick={() => setIsEditModalOpen(true)} className="mt-4">
@@ -69,7 +70,7 @@ const Profile = () => {
       <EditProfileModal
         isOpen={isEditModalOpen}
         onClose={() => setIsEditModalOpen(false)}
-        // initialData={user?.user}
+        // initialData={singleUser}
         // onSave={(data: any) => {
         //   console.log("Profile updated:", data);
         //   dispatch(setUser(data));
